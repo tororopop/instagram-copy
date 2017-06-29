59f4f8b50b6cef58798c1abc544a41511b01adb5
m.murai <murai@mmurai-no-MacBook-Pro.local>
2017-06-29 15:04:40 +0900

    “20170629”

--- config/application.rb
+++ config/application.rb
@@ -2,13 +2,18 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+require 'carrierwave'
+
+if defined?(Bundler)
+  # Bundler stuff
+end
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module InstagramCopy
   class Application < Rails::Application
-    config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
