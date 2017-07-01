2624ce809569033f71e35e44a02423a2797f7e2a
m.murai <murai@mmurai-no-MacBook-Pro.local>
2017-07-01 11:09:21 +0900

    20170701

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -13,6 +13,7 @@ class PicturesController < ApplicationController
 
   def create
     @picture = Picture.new(pictures_params)
+    @picture.user_id = current_user.id
     if @picture.save
       redirect_to pictures_path, notice: "画像を投稿しました！"
       NoticeMailer.sendmail_picture(@picture).deliver
