842d883a14fa94bbaacc8f792a549817a3a07ca2
m.murai <murai@mmurai-no-MacBook-Pro.local>
2017-07-02 16:32:51 +0900

    20170702

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -29,6 +29,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
+  process resize_to_limit: [200, 200]
+
   # Create different versions of your uploaded files:
   version :thumb do
     process resize_to_fit: [30, 30]
