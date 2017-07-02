f692afad9c64bf2d94b2237697f8348db21193ef
m.murai <murai@mmurai-no-MacBook-Pro.local>
2017-07-02 11:49:02 +0900

    20170702

--- Gemfile
+++ Gemfile
@@ -64,3 +64,7 @@ group :development, :test do
   gem 'pry-rails'
   gem 'better_errors'
 end
+
+gem 'omniauth'
+gem 'omniauth-twitter'
+gem 'omniauth-facebook'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def profile_img(user)
+    return image_tag(user.avatar, alt: user.name) if user.avatar?
+
+    unless user.provider.blank?
+      img_url = user.image_url
+    else
+      img_url = 'no_image.png'
+    end
+    image_tag(img_url, :size => "200x200", alt: user.name)
+  end
+
+  def profile_img_thumb(user)
+    return image_tag(user.avatar.thumb, alt: user.name) if user.avatar?
+
+    unless user.provider.blank?
+      img_url = user.image_url
+    else
+      img_url = 'no_image.png'
+    end
+    image_tag(img_url, :size => "50x50", alt: user.name)
+  end
 end
